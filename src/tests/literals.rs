a8086d18497fb4664fc4914fc0f4ce4f682bf3d2
Mark Ross <p17mross@beaumont.school>
2023-02-27 22:20:37 +0000

    Added more tests for number literals, Added more edge cases to number tests Changed assert macros to functions

--- src/tests/literals.rs
+++ src/tests/literals.rs
@@ -1,64 +1,94 @@
 //! Tests for value literals and comments
 
+use super::*;
+
 /// Tests that number literals are parsed, and that invalid number literals are rejected
 #[test]
 fn test_number_literals() {
-    // Parsing number literals
-    assert_parses!(10);
-    assert_parses!(10.0);
-    assert_parses!(0x01234567890);
-    assert_parses!(0b010);
-    assert_parses!(0o012345670);
-    assert_parses!(123456789n);
-    assert_parses!(10n);
-    assert_parses!(0x100n);
-    assert_parses!(0b10n);
-    assert_parses!(0o0123n);
+    assert_parses("10");
+    assert_parses("10.0");
 
-    assert_not_parses_str!("20.0n"); // BigInt literal with decimal point
-    assert_not_parses_str!("10h"); // Number literal with an identifier after
-    assert_not_parses_str!("012n"); // Implicit octal BigInt literal
-    assert_not_parses_str!("10na"); // BigInt literal with a character after
-    assert_not_parses_str!("0xGH"); // Hex literal with wrong characters
-    assert_not_parses_str!("0b0123"); // Binary literal with wrong characters
-    assert_not_parses_str!("0o123456789"); // Octal literal with wrong characters
+    assert_parses("0x01234567890");
+    assert_parses("0b010");
+    assert_parses("0o012345670");
+
+    assert_parses("123456789n");
+    assert_parses("10n");
+    assert_parses("0x100n");
+    assert_parses("0b10n");
+    assert_parses("0o0123n");
+    
+    assert_parses("1_000"); // Underscores as separators
+    assert_parses("1_000n");
+    assert_parses("1_000.000_001");
+
+    assert_parses(".10"); // Leading decimal point
+    assert_parses("10."); // Trailing decimal point
+
+    assert_not_lexes("20.0n"); // BigInt literal with decimal point
+    assert_not_lexes("10h"); // Number literal with an identifier after
+    assert_not_lexes("012n"); // Implicit octal BigInt literal
+    assert_not_lexes("10na"); // BigInt literal with a character after
+    assert_not_lexes("0xGH"); // Hex literal with wrong characters
+    
+    assert_not_lexes("10_"); // Trailing underscore
+    assert_not_lexes("10_.0"); // Underscore before decimal point
+    assert_not_lexes("10._0"); // Underscore after decimal point
+    assert_not_lexes("10_n"); // Underscore directly before n in BigInt literalW
+    assert_not_lexes("0x.5"); // Decimal point after 0x
+    
+    assert_not_lexes("0x"); // Missing hex digits
+    assert_not_lexes("0o"); // Missing octal digits
+    assert_not_lexes("0b"); // Missing binary digits
+    assert_not_lexes("0xn"); // Missing hex digits
+    assert_not_lexes("0on"); // Missing octal digits
+    assert_not_lexes("0bn"); // Missing binary digits
+    assert_not_lexes("0x_10"); // Underscore before digits
+    assert_not_lexes("0o_10"); // Underscore before digits
+    assert_not_lexes("0b_10"); // Underscore before digits
+    
+
+    assert_lexes_only("0b0123"); // Binary literal with wrong characters - should be lexed as '0b01' '23'
+    assert_lexes_only("0o123456789"); // Octal literal with wrong characters - should be lexed as '0o1234567' '89' 
+    assert_lexes_only("10.10.10"); // Number with multiple decimal points - should be lexed as '10.10' '.10'
+    assert_lexes_only("10..5"); // Number with consecutive decimal points - should be lexed as '10.' '.5'
 }
 
 /// Tests that string literals are parsed, and that invalid string literals are rejected
 #[test]
 fn test_string_literals() {
-    assert_parses_str!(r#" "Some string" "#);
-    assert_parses_str!(r#" "Some string with \b \f \n \r \t \v " "#);
-    assert_parses_str!(r#" "Single quotes in double quoted string: '' `` " "#);
-    assert_parses_str!(r#" 'Double quotes in single quoted string: "" `` ' "#);
-    assert_parses_str!(r#" `Double and single quotes in backtick string "" '' ` "#);
-    assert_parses_str!(r#" `Newlines in a backtick quoted string
+    assert_parses(r#" "Some string" "#);
+    assert_parses(r#" "Some string with \b \f \n \r \t \v " "#);
+    assert_parses(r#" "Single quotes in double quoted string: '' `` " "#);
+    assert_parses(r#" 'Double quotes in single quoted string: "" `` ' "#);
+    assert_parses(r#" `Double and single quotes in backtick string "" '' ` "#);
+    assert_parses(r#" `Newlines in a backtick quoted string
     ` "#);
-    assert_parses_str!(r#" "Escaped double quotes: \" " "#);
-    assert_parses_str!(r#" 'Escaped single quotes: \' ' "#);
-    assert_parses_str!(r#" `Escaped backticks: \` ` "#);
+    assert_parses(r#" "Escaped double quotes: \" " "#);
+    assert_parses(r#" 'Escaped single quotes: \' ' "#);
+    assert_parses(r#" `Escaped backticks: \` ` "#);
     
 
-    assert_not_parses_str!(r#" "Newline in double quoted string
+    assert_not_lexes(r#" "Newline in double quoted string
     " "#);
-    assert_not_parses_str!(r#" 'Newline in single quoted string
+    assert_not_lexes(r#" 'Newline in single quoted string
     ' "#);
-    assert_not_parses_str!(r#" "Unclosed double quote string "#);
-    assert_not_parses_str!(r#" 'Unclosed single quote string "#);
-    assert_not_parses_str!(r#" `Unclosed backtick string "#);
+    assert_not_lexes(r#" "Unclosed double quote string "#);
+    assert_not_lexes(r#" 'Unclosed single quote string "#);
+    assert_not_lexes(r#" `Unclosed backtick string "#);
 }
 
 /// Tests that 
 #[test]
 fn test_comments() {
-    assert_parses_str!(r#" // A line comment with no newline "#);
-    assert_parses_str!(r#" // A line comment with a trailing newline 
+    assert_parses(r#" // A line comment with no newline "#);
+    assert_parses(r#" // A line comment with a trailing newline 
     "#);
-    assert_parses_str!(r#" // A line comment // This is still commented "#);
-    assert_parses_str!(r#" /* A block comment */ "#);
-    assert_parses_str!(r#" /* A block comment
+    assert_parses(r#" // A line comment // This is still commented "#);
+    assert_parses(r#" /* A block comment */ "#);
+    assert_parses(r#" /* A block comment
     with a newline */ "#);
 
-    assert_not_parses_str!(r#" /* A block comment with no end "#);
+    assert_not_lexes(r#" /* A block comment with no end "#);
 
 }
--- src/tests/mod.rs
+++ src/tests/mod.rs
@@ -1,41 +1,46 @@
 #![cfg(test)]
 
-/// Asserts that a given program parses. The program does not need to be quoted, but this means newlines will be ignored outside of string literals. 
-/// For testing code with significant newlines, such as testing semicolon insertion, uss the [`assert_parses_str!`] macro instead.
-macro_rules! assert_parses {
-    ($($code: tt)*) => {
-        assert_parses_str!(stringify!($($code)*))
-    };
-}
+use crate::{Program, parser::Parser};
+use crate::engine::{Gc, ProgramSource};
+use crate::lexer::{Lexer};
 
-/// Asserts that a given string parses
-macro_rules! assert_parses_str {
-    ($code: expr) => {{
-        use crate::Program;
-        Program::from_console($code).expect("Program should have parsed");
-    }};
-}
-
-/// Asserts that a given program does not parse. The same restrictions surrounding newlines apply as with [`assert_parses!`].
-macro_rules! assert_not_parses {
-    ($($code: tt)*) => {
-        assert_not_parses_str!(stringify!($($code)*))
-    };
-}
-
-/// Asserts that a given string parses
-macro_rules! assert_not_parses_str {
-    ($code: expr) => {{
-        use crate::Program;
-        Program::from_console($code).expect_err("Program should not have parsed");
-    }};
-}
-
-// Mods need to be after macro definitions or the modules will not compile
 mod literals;
+
+/// Asserts that a given program lexes and parses
+fn assert_parses(code: &str) {
+    Program::from_console(code).expect("Program should have parsed");
+}
+
+/// Asserts that a given program does not lex
+fn assert_not_lexes(code: &str) {
+    let program = Gc::new(Program {
+        source: ProgramSource::Console,
+        program: code.chars().collect(),
+        ast: None,
+    });
+    let lexer = Lexer::new();
+    lexer.lex(&program).expect_err("Program should not have lexed");
+}
+
+/// Asserts that a given program lexes but does not parse.
+/// This is useful for constructs which can be tokenised but which are not semantically valid, e.g. 'id id'
+fn assert_lexes_only(code: &str) {
+    let program = Gc::new(Program {
+        source: ProgramSource::Console,
+        program: code.chars().collect(),
+        ast: None,
+    });
+    let lexer = Lexer::new();
+    let tokens = lexer.lex(&program).expect("Program should have lexed");
+    Parser::parse(program, tokens).expect_err("Program should not have parsed");
+}
+
 
 /// Asserts that an empty program parses
 #[test]
 fn test_empty_program() {
-    assert_parses!();
+    assert_parses("");
+    assert_parses(" ");
+    assert_parses("
+    ");
 }
